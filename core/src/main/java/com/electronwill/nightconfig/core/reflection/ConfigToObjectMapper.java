16e157d91c45513f30247796a894f2b438303db2
TheElectronWill <theelectronwill@gmail.com>
2016-12-18 23:14:38 +0100

    Documentation + add parameter respectTransientModifier to ObjectToConfigMapper

--- core/src/main/java/com/electronwill/nightconfig/core/reflection/ConfigToObjectMapper.java
+++ core/src/main/java/com/electronwill/nightconfig/core/reflection/ConfigToObjectMapper.java
@@ -11,18 +11,37 @@ import java.util.Map;
  *
  * @author TheElectronWill
  */
-public class ConfigToObjectMapper {
+public final class ConfigToObjectMapper {
 
 	protected final Map<Class<?>, ValueConverter<?, ?>> conversionMap;
 
+	/**
+	 * Creates a new ConfigToObjectMapper with an initially empty conversion map.
+	 */
 	public ConfigToObjectMapper() {
 		this(new HashMap<>());
 	}
 
+	/**
+	 * Creates a new ConfigToObjectMapper with the specified conversion map.
+	 *
+	 * @param conversionMap the map to use for converting values
+	 */
 	public ConfigToObjectMapper(Map<Class<?>, ValueConverter<?, ?>> conversionMap) {
 		this.conversionMap = conversionMap;
 	}
 
+	/**
+	 * Maps a config to a newly created object of class {@code objectClass}. This retrieves each config's
+	 * first-level value and writes it to the corresponding object's field. A field corresponds to a
+	 * config's entry when it has the same name.
+	 *
+	 * @param config      the config to map
+	 * @param objectClass the class of the object
+	 * @param <T>         the object's type
+	 * @return a new instance of {@code objectClass} with its fields set according to the config.
+	 * @throws ReflectiveOperationException
+	 */
 	public <T> T map(Config config, Class<T> objectClass) throws ReflectiveOperationException {
 		Constructor<T> constructor = objectClass.getDeclaredConstructor();//constructor with no parameters
 		if (!constructor.isAccessible()) {
@@ -33,6 +52,13 @@ public class ConfigToObjectMapper {
 		return object;
 	}
 
+	/**
+	 * Maps a config to an object. This retrieves each config's first-level value and writes it to the
+	 * corresponding object's field. A field corresponds to a config's entry when it has the same name.
+	 *
+	 * @param config the config to map
+	 * @param object the object to map the config to
+	 */
 	public void map(Config config, Object object) throws ReflectiveOperationException {
 		Class<?> c = object.getClass();
 		Map<String, Object> map = config.asMap();
@@ -40,21 +66,25 @@ public class ConfigToObjectMapper {
 			String key = entry.getKey();
 			Object value = entry.getValue();//may be null!
 
+			// Enforces access if needed
 			Field correspondingField = c.getDeclaredField(key);
 			if (!correspondingField.isAccessible()) {
 				correspondingField.setAccessible(true);
 			}
 
+			// If the value is null, no conversion is possible
 			if (value == null) {
 				correspondingField.set(object, value);
 				continue;
 			}
 
+			// Converts the value if there is a converter for it
 			ValueConverter converter = conversionMap.get(value.getClass());
 			if (converter != null && converter.canConvert(value)) {
 				value = converter.convert(value);
 			}
 
+			// Sets the field value, directly or not
 			if (value instanceof Config && !Config.class.isAssignableFrom(correspondingField.getType())) {
 				//Config value but not Config field => maps to object
 				if (correspondingField.get(object) == null) {//field is null -> try to create it
@@ -63,17 +93,33 @@ public class ConfigToObjectMapper {
 				} else {//field is not null -> map values
 					map((Config)value, correspondingField.get(object));//map to object recursively
 				}
-			} else {
+			} else {//simple value
 				correspondingField.set(object, value);
 			}
 		}
 	}
 
+	/**
+	 * Adds a conversion to apply to objects' fields.
+	 *
+	 * @param typeToConvert     the class of the type of field to apply this conversion to
+	 * @param conversionChecker the object that decides if the conversion applies
+	 * @param conversionApplier the object that applies the conversion
+	 * @param <T>               the type to convert
+	 */
 	public <T> void addConversion(Class<T> typeToConvert, ConversionChecker<T> conversionChecker,
 								  ConversionApplier<T, ?> conversionApplier) {
 		conversionMap.put(typeToConvert, new ValueConverter<>(conversionChecker, conversionApplier));
 	}
 
+	/**
+	 * Removes a conversion to apply to objects' fields.
+	 *
+	 * @param typeToConvert     the class of the type of field to apply this conversion to
+	 * @param conversionChecker the object that decides if the conversion applies
+	 * @param conversionApplier the object that applies the conversion
+	 * @param <T>               the type to convert
+	 */
 	public <T> void removeConversion(Class<T> typeToConvert, ConversionChecker<T> conversionChecker,
 									 ConversionApplier<T, ?> conversionApplier) {
 		ValueConverter converter = conversionMap.get(typeToConvert);
--- core/src/main/java/com/electronwill/nightconfig/core/reflection/ObjectToConfigMapper.java
+++ core/src/main/java/com/electronwill/nightconfig/core/reflection/ObjectToConfigMapper.java
@@ -3,6 +3,7 @@ package com.electronwill.nightconfig.core.reflection;
 import com.electronwill.nightconfig.core.Config;
 import com.electronwill.nightconfig.core.MapConfig;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,31 +12,75 @@ import java.util.Map;
  *
  * @author TheElectronWill
  */
-public class ObjectToConfigMapper {
+public final class ObjectToConfigMapper {
 
-	protected final Map<Class<?>, ValueConverter<?, ?>> conversionMap;
+	private final Map<Class<?>, ValueConverter<?, ?>> conversionMap;
+	private final boolean respectTransientModifier;
 
+	/**
+	 * Creates a new ObjectToConfigMapper with an initially empty conversion map, and that respects the
+	 * transient modified.
+	 */
 	public ObjectToConfigMapper() {
-		this(new HashMap<>());
+		this(new HashMap<>(), true);
 	}
 
-	public ObjectToConfigMapper(Map<Class<?>, ValueConverter<?, ?>> conversionMap) {
+	/**
+	 * Creates a new ObjectToConfigMapper with the specified conversion map.
+	 *
+	 * @param conversionMap            the map to use for converting values
+	 * @param respectTransientModifier true to respect the transient modifiers of fields, that is, to
+	 *                                 ignore the transient fields; false to ignore the transient modifier
+	 *                                 and to treat all the fields
+	 */
+	public ObjectToConfigMapper(Map<Class<?>, ValueConverter<?, ?>> conversionMap, boolean respectTransientModifier) {
 		this.conversionMap = conversionMap;
+		this.respectTransientModifier = respectTransientModifier;
 	}
 
-	public void map(Object object, Config config) throws IllegalAccessException {
+	/**
+	 * Maps an object to a config. This retrieves the value of each object's field and puts it in the
+	 * config. If this ObjectToConfigMapper respects the transient modified, the fields that have a
+	 * transient modifier will not be written to the config.
+	 *
+	 * @param object the object to map
+	 * @param config the config to map the object to
+	 * @throws SecurityException if a field of the object isn't accessible and the request to access it is
+	 *                           denied
+	 */
+	public void map(Object object, Config config) {
 		Class<?> c = object.getClass();
 		Map<String, Object> map = config.asMap();
 		for (Field field : c.getDeclaredFields()) {
-			String name = field.getName();
-			Object value = field.get(object);//may be null!
+			// Checks if it is a transient field
+			final int modifiers = field.getModifiers();
+			if (respectTransientModifier && Modifier.isTransient(modifiers)) {
+				continue;//respects the transient modifier: ignore the field
+			}
 
+			// Enforces access if needed
+			if (!field.isAccessible()) {
+				field.setAccessible(true);//may throw a SecurityException
+			}
+
+			// Gets the field's value
+			Object value;
+			try {
+				value = field.get(object);//may be null!
+			} catch (IllegalAccessException e) {
+				//This does not happen in practice, because we've done field.setAccessible(true)
+				throw new IllegalStateException(e);
+			}
+
+			// Converts the value if there is a converter for it
 			ValueConverter converter = conversionMap.get(field.getType());
 			if (converter != null && converter.canConvert(value)) {
 				value = converter.convert(value);
 			}
 
-			if (value != null && !config.supportsType(value.getClass())) {// unsupported by config => consider that it's a compound object
+			// Puts the value in the config, directly or field by field
+			final String name = field.getName();
+			if (value != null && !config.supportsType(value.getClass())) {//Unsupported by config => consider that it's a compound object
 				Config correspondingConfig;
 				if (((map.get(name) instanceof Config))) {//if a subconfig with the correct name already exists, use it.
 					correspondingConfig = (Config)map.get(name);
@@ -50,11 +95,27 @@ public class ObjectToConfigMapper {
 		}
 	}
 
+	/**
+	 * Adds a conversion to apply to objects' fields.
+	 *
+	 * @param typeToConvert     the class of the type of field to apply this conversion to
+	 * @param conversionChecker the object that decides if the conversion applies
+	 * @param conversionApplier the object that applies the conversion
+	 * @param <T>               the type to convert
+	 */
 	public <T> void addConversion(Class<T> typeToConvert, ConversionChecker<T> conversionChecker,
 								  ConversionApplier<T, ?> conversionApplier) {
 		conversionMap.put(typeToConvert, new ValueConverter<>(conversionChecker, conversionApplier));
 	}
 
+	/**
+	 * Removes a conversion to apply to objects' fields.
+	 *
+	 * @param typeToConvert     the class of the type of field to apply this conversion to
+	 * @param conversionChecker the object that decides if the conversion applies
+	 * @param conversionApplier the object that applies the conversion
+	 * @param <T>               the type to convert
+	 */
 	public <T> void removeConversion(Class<T> typeToConvert, ConversionChecker<T> conversionChecker,
 									 ConversionApplier<T, ?> conversionApplier) {
 		ValueConverter converter = conversionMap.get(typeToConvert);
