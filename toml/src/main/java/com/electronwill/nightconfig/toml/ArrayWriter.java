cfff25e11ed49c1abfe1773253cf1553ccd82030
TheElectronWill <theelectronwill@gmail.com>
2017-02-25 15:29:43 +0100

    toml: implement simple ArrayWriter

--- toml/src/main/java/com/electronwill/nightconfig/toml/ArrayWriter.java
+++ toml/src/main/java/com/electronwill/nightconfig/toml/ArrayWriter.java
@@ -1,13 +1,31 @@
 package com.electronwill.nightconfig.toml;
 
 import com.electronwill.nightconfig.core.serialization.CharacterOutput;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * @author TheElectronWill
  */
-public class ArrayWriter {
-	static void writeArray(List<?> values, CharacterOutput output) {
+class ArrayWriter {
+	private static final char[] EMPTY_ARRAY = {'[', ']'}, ELEMENT_SEPARATOR = {',', ' '};
 
+	static void writeArray(List<?> values, CharacterOutput output) {
+		if (values.isEmpty()) {
+			output.write(EMPTY_ARRAY);
+			return;
+		}
+		output.write('[');
+		Iterator<?> it = values.iterator();
+		while (true) {
+			Object next = it.next();
+			output.write(next.toString());
+			if (it.hasNext()) {
+				output.write(ELEMENT_SEPARATOR);
+			} else {
+				output.write(']');
+				return;
+			}
+		}
 	}
 }
