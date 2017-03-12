ab81937570506caa1e86d6606dd447b161008108
TheElectronWill <theelectronwill@gmail.com>
2017-03-12 22:42:08 +0100

    yaml: Implement ConfigParser, ConfigWriter

---
+++ yaml/src/main/java/com/electronwill/nightconfig/yaml/YamlParser.java
@@ -0,0 +1,33 @@
+package com.electronwill.nightconfig.yaml;
+
+import com.electronwill.nightconfig.core.serialization.ConfigParser;
+import java.io.Reader;
+import java.util.Map;
+import org.yaml.snakeyaml.LoaderOptions;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * @author TheElectronWill
+ */
+public final class YamlParser implements ConfigParser<YamlConfig> {
+	private final Yaml yaml;
+
+	public YamlParser() {this(new Yaml());}
+
+	public YamlParser(Yaml yaml) {this.yaml = yaml;}
+
+	public YamlParser(LoaderOptions options) {this(new Yaml(options));}
+
+	@Override
+	public YamlConfig parseConfig(Reader reader) {
+		YamlConfig config = new YamlConfig();
+		parseConfig(reader, config);
+		return config;
+	}
+
+	@Override
+	public void parseConfig(Reader reader, YamlConfig destination) {
+		Map<String, Object> map = yaml.loadAs(reader, Map.class);
+		destination.asMap().putAll(map);
+	}
+}
---
+++ yaml/src/main/java/com/electronwill/nightconfig/yaml/YamlWriter.java
@@ -0,0 +1,26 @@
+package com.electronwill.nightconfig.yaml;
+
+import com.electronwill.nightconfig.core.Config;
+import com.electronwill.nightconfig.core.serialization.ConfigWriter;
+import java.io.IOException;
+import java.io.Writer;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * @author TheElectronWill
+ */
+public final class YamlWriter implements ConfigWriter<Config> {
+	private final Yaml yaml;
+
+	public YamlWriter() {this(new Yaml());}
+
+	public YamlWriter(Yaml yaml) {this.yaml = yaml;}
+
+	public YamlWriter(DumperOptions options) {this(new Yaml(options));}
+
+	@Override
+	public void writeConfig(Config config, Writer writer) throws IOException {
+		yaml.dump(ConfigUnwrapper.unwrap(config), writer);
+	}
+}
