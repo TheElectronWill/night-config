c78437501ebdf9e674c228396a8928d31239da29
TheElectronWill <theelectronwill@gmail.com>
2017-03-18 20:53:30 +0100

    core: SimpleConfig now use a Predicate instead of a custom interface

--- core/src/main/java/com/electronwill/nightconfig/core/SimpleConfig.java
+++ core/src/main/java/com/electronwill/nightconfig/core/SimpleConfig.java
@@ -1,38 +1,16 @@
 package com.electronwill.nightconfig.core;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.function.Predicate;
 
 /**
- * A simple configuration that allows the user to specify a support strategy.
+ * A simple configuration that allows the user to specify which types of value it supports.
  */
 public final class SimpleConfig extends MapConfig {
-
-	private static final Set<Class<?>> BASIC_TYPES = new HashSet<>();
-	private static final Set<Class<?>> BASIC_EXTENSIBLE_TYPES = new HashSet<>();
-
-	static {
-		BASIC_TYPES.add(Integer.class);
-		BASIC_TYPES.add(Long.class);
-		BASIC_TYPES.add(Float.class);
-		BASIC_TYPES.add(Double.class);
-		BASIC_TYPES.add(Boolean.class);
-		BASIC_TYPES.add(String.class);
-		BASIC_TYPES.add(List.class);
-		BASIC_TYPES.add(Config.class);
-
-		BASIC_EXTENSIBLE_TYPES.add(List.class);
-		BASIC_EXTENSIBLE_TYPES.add(Config.class);
-	}
+	private final Predicate<Class<?>> supportPredicate;
 
 	/**
-	 * Supports anything: supportsType(type) returns {@code true} for all type.
-	 */
-	public static final SupportStrategy STRATEGY_SUPPORT_ALL = type -> true;
-
-	/**
-	 * Supports the following types:
+	 * Creates a new SimpleConfig that supports the following types:
 	 * <ul>
 	 * <li>Integer, Long, Float and Double
 	 * <li>Boolean
@@ -40,88 +18,36 @@ public final class SimpleConfig extends MapConfig {
 	 * <li>List and all its implementations
 	 * <li>Config and all its implementations
 	 * </ul>
-	 */
-	public static final SupportStrategy STRATEGY_SUPPORT_BASIC = new SimpleSupportStrategy(
-			BASIC_TYPES, BASIC_EXTENSIBLE_TYPES);
-
-	private final SupportStrategy supportStrategy;
-
-	/**
-	 * Creates a new SimpleConfig that uses the basic SupportStrategy.
-	 * <p>
-	 * It supports the following types:
-	 * <ul>
-	 * <li>Integer, Long, Float and Double
-	 * <li>Boolean
-	 * <li>String
-	 * <li>List and all its implementations
-	 * <li>Config and all its implementations
-	 * </ul>
-	 *
-	 * @see SimpleConfig#STRATEGY_SUPPORT_BASIC
 	 */
 	public SimpleConfig() {
-		this(STRATEGY_SUPPORT_BASIC);
+		this.supportPredicate = type -> type == Integer.class
+				|| type == Long.class
+				|| type == Float.class
+				|| type == Double.class
+				|| type == Boolean.class
+				|| type == String.class
+				|| List.class.isAssignableFrom(type)
+				|| Config.class.isAssignableFrom(type);
 	}
 
 	/**
-	 * Creates a new SimpleConfig that uses the specified SupportStrategy.
+	 * Creates a new SimpleConfig that uses the specified Predicate to determines which types it
+	 * supports.
 	 *
-	 * @param supportStrategy the SupportStrategy that decides which classes of values are
-	 *                        supported by the config
-	 * @see SimpleConfig#STRATEGY_SUPPORT_BASIC
-	 * @see SimpleConfig#STRATEGY_SUPPORT_ALL
+	 * @param supportPredicate the Predicate that returns true when the class it's given is
+	 *                         supported by the config
 	 */
-	public SimpleConfig(SupportStrategy supportStrategy) {
-		this.supportStrategy = supportStrategy;
+	public SimpleConfig(Predicate<Class<?>> supportPredicate) {
+		this.supportPredicate = supportPredicate;
 	}
 
 	@Override
 	public boolean supportsType(Class<?> type) {
-		return supportStrategy.supportsType(type);
+		return supportPredicate.test(type);
 	}
 
 	@Override
 	protected SimpleConfig createSubConfig() {
-		return new SimpleConfig(supportStrategy);
-	}
-
-	/**
-	 * An implementation of SupportStrategy based on two sets: one for the types that are
-	 * supported strictly, one for the types that are supported with their subtypes.
-	 */
-	public static final class SimpleSupportStrategy implements SupportStrategy {
-		private final Set<Class<?>> supportedTypes, extensibleSupportedTypes;
-
-		/**
-		 * Creates a new SimpleSupportStrategy.
-		 *
-		 * @param supportedTypes           the set of the types that are stricly supported (their
-		 *                                 subtypes aren't supported)
-		 * @param extensibleSupportedTypes the set of the types that are supported with their
-		 *                                 subtypes.
-		 */
-		public SimpleSupportStrategy(Set<Class<?>> supportedTypes,
-									 Set<Class<?>> extensibleSupportedTypes) {
-			this.supportedTypes = supportedTypes;
-			this.extensibleSupportedTypes = extensibleSupportedTypes;
-		}
-
-		@Override
-		public boolean supportsType(Class<?> type) {
-			if (supportedTypes.contains(type)) { return true; }
-			for (Class<?> supportedType : extensibleSupportedTypes) {
-				if (supportedType.isAssignableFrom(type)) { return true; }
-			}
-			return false;
-		}
-	}
-
-	/**
-	 * Checks whether a type is supported (by the config) or not.
-	 */
-	@FunctionalInterface
-	public static interface SupportStrategy {
-		boolean supportsType(Class<?> type);
+		return new SimpleConfig(supportPredicate);
 	}
 }
