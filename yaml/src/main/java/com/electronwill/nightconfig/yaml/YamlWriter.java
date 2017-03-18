3bd727c108f01d46d7505cb7a5a3702fbbe51bc1
TheElectronWill <theelectronwill@gmail.com>
2017-03-18 21:12:14 +0100

    yaml: Use a Transforming map instead of the ConfigUnwrapper

--- yaml/src/main/java/com/electronwill/nightconfig/yaml/YamlWriter.java
+++ yaml/src/main/java/com/electronwill/nightconfig/yaml/YamlWriter.java
@@ -2,8 +2,10 @@ package com.electronwill.nightconfig.yaml;
 
 import com.electronwill.nightconfig.core.Config;
 import com.electronwill.nightconfig.core.io.ConfigWriter;
+import com.electronwill.nightconfig.core.utils.TransformingMap;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Map;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
@@ -27,6 +29,18 @@ public final class YamlWriter implements ConfigWriter<Config> {
 
 	@Override
 	public void writeConfig(Config config, Writer writer) throws IOException {
-		yaml.dump(ConfigUnwrapper.unwrap(config), writer);
+		Map<String, Object> unwrappedMap = unwrap(config);
+		yaml.dump(unwrappedMap, writer);
+	}
+
+	private static Map<String, Object> unwrap(Config config) {
+		return new TransformingMap<>(config.asMap(), YamlWriter::unwrap, v -> v, v -> v);
+	}
+
+	private static Object unwrap(Object value) {
+		if (value instanceof Config) {
+			return unwrap((Config)value);
+		}
+		return value;
 	}
 }
