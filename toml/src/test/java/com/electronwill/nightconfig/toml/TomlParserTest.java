c431ffd5d8b3d8ad2bb490cf8defd6aed0bdb021
TheElectronWill <theelectronwill@gmail.com>
2017-04-01 23:37:07 +0200

    toml: Add tests about the definition of subtables and duplicate keys

--- toml/src/test/java/com/electronwill/nightconfig/toml/TomlParserTest.java
+++ toml/src/test/java/com/electronwill/nightconfig/toml/TomlParserTest.java
@@ -1,15 +1,30 @@
 package com.electronwill.nightconfig.toml;
 
+import com.electronwill.nightconfig.core.io.ParsingException;
 import java.io.StringReader;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 /**
  * @author TheElectronWill
  */
 public class TomlParserTest {
 
+	private static void assertThrows(Class<? extends Exception> exceptionClass,
+									 Executable executable) {
+		Exception exception = Assertions.assertThrows(exceptionClass, executable);
+		System.out.println("Got expected exception: " + exception);
+	}
+
+	private static void parseAndPrint(String tomlString) {
+		TomlParser parser = new TomlParser();
+		TomlConfig parsed = parser.parseConfig(new StringReader(tomlString));
+		System.out.println("parsed: " + parsed);
+	}
+
 	@Test
-	public void readFromString() {
+	public void readValidString() {
 		String toml = "string = \"value\"\n"
 					  + "integer = 2\n"
 					  + "long = 1234567890000\n"
@@ -17,11 +32,44 @@ public class TomlParserTest {
 					  + "bool_array=[true, false, true, false,] # comment\n"
 					  + "[table] #comment	\n"
 					  + "	'key' = '\"literal string\"\\n\\t'\n"
+					  + "[table.subTable.subDefinedFirst]\n"
+					  + "   test = 'this is valid TOML'\n"
 					  + "[table.subTable]\n"
-					  + "    \"subkey\"=2017-02-25T12:00:01.123456789   \n"
-					  + "\r\n# 'key' = 0.2";
-		TomlParser parser = new TomlParser();
-		TomlConfig parsed = parser.parseConfig(new StringReader(toml));
-		System.out.println("parsed: " + parsed);
+					  + "    \"subkey\"=2017-02-25T12:00:01.123456789   \n";
+		parseAndPrint(toml);
 	}
+
+	@Test
+	public void readInvalidString() {
+		assertThrows(ParsingException.class, this::testAlreadyDefinedTable);
+		assertThrows(ParsingException.class, this::testAlreadyDefinedTable2);
+		assertThrows(ParsingException.class, this::testAlreadyDefinedKey);
+	}
+
+	private void testAlreadyDefinedTable() {
+		String toml = "string = \"value\"\n"
+					  + "[table.subTable.subDefinedFirst]\n"
+					  + "   test = 'success'\n"
+					  + "[table.subTable]\n"
+					  + "    subDefinedFirst = {}"
+					  + "\r\n# 'key' = 0.2";
+		parseAndPrint(toml);
+	}
+
+	private void testAlreadyDefinedTable2() {
+		String toml = "string = \"value\"\n"
+					  + "[table.subTable]\n"
+					  + "   test = 'success'\n"
+					  + "[table.subTable]\n"
+					  + "   subDefinedFirst = {}";
+		parseAndPrint(toml);
+	}
+
+	private void testAlreadyDefinedKey() {
+		String toml = "string = \"value\"\n"
+					  + "test = 'success'\n"
+					  + "test = 'already defined!'";
+		parseAndPrint(toml);
+	}
+
 }
