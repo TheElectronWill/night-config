ef507656067ddf16704aa9e14f9b119044a2a3c7
TheElectronWill <theelectronwill@gmail.com>
2017-04-04 17:19:26 +0200

    toml: Toml: Make constants private and reorder methods

--- toml/src/main/java/com/electronwill/nightconfig/toml/Toml.java
+++ toml/src/main/java/com/electronwill/nightconfig/toml/Toml.java
@@ -9,10 +9,10 @@ import com.electronwill.nightconfig.core.io.Utils;
  */
 final class Toml {
 
-	static final char[] WHITESPACE_OR_NEWLINE = {'\t', ' ', '\n', '\r'};
-	static final char[] WHITESPACE = {'\t', ' '};
-	static final char[] NEWLINE = {'\n'};
-	static final char[] FORBIDDEN_IN_ALL_BARE_KEYS = {'.', '[', ']', '#', '='};
+	private static final char[] WHITESPACE_OR_NEWLINE = {'\t', ' ', '\n', '\r'};
+	private static final char[] WHITESPACE = {'\t', ' '};
+	private static final char[] NEWLINE = {'\n'};
+	private static final char[] FORBIDDEN_IN_ALL_BARE_KEYS = {'.', '[', ']', '#', '='};
 
 	/**
 	 * Returns the next "useful" character. Skips comments, spaces and newlines.
@@ -26,11 +26,9 @@ final class Toml {
 		return next;
 	}
 
-	static char readNonSpaceChar(CharacterInput input) {
-		return input.readCharAndSkip(WHITESPACE);
-	}
-
-
+	/**
+	 * Returns the next "useful" character. Skips comments, spaces and newlines.
+	 */
 	static int readUseful(CharacterInput input) {
 		int next = input.readAndSkip(WHITESPACE_OR_NEWLINE);
 		while (next == '#') {
@@ -40,6 +38,16 @@ final class Toml {
 		return next;
 	}
 
+	/**
+	 * Reads the next non-space character. Doesn't skip comments.
+	 */
+	static char readNonSpaceChar(CharacterInput input) {
+		return input.readCharAndSkip(WHITESPACE);
+	}
+
+	/**
+	 * Reads the next non-space character. Doesn't skip comments.
+	 */
 	static int readNonSpace(CharacterInput input) {
 		return input.readAndSkip(WHITESPACE);
 	}
