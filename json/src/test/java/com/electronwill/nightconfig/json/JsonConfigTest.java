ed501a4cd5875b8cd3ffea75af475f229c9581cd
TheElectronWill <theelectronwill@gmail.com>
2017-04-11 19:32:17 +0200

    json: Add more tests

--- json/src/test/java/com/electronwill/nightconfig/json/JsonConfigTest.java
+++ json/src/test/java/com/electronwill/nightconfig/json/JsonConfigTest.java
@@ -4,7 +4,13 @@ import com.electronwill.nightconfig.core.Config;
 import com.electronwill.nightconfig.core.SimpleConfig;
 import com.electronwill.nightconfig.core.serialization.FileConfig;
 import com.electronwill.nightconfig.core.serialization.WriterOutput;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
@@ -26,11 +32,20 @@ public class JsonConfigTest {
 		config.setValue("float", 0.123456f);
 		config.setValue("double", 0.123456d);
 		config.setValue("config", config2);
-		config.setValue("list", Arrays.asList("a", "b", 3, null, true, false, 17.5));
+		config.setValue("list", Arrays.asList("a", "b", 3, true, null, false, 17.5));
 		config.setValue("null", null);
 	}
 
 	private final File file = new File("test.json");
+
+	@Test
+	public void testWriteThenRead() throws IOException {
+		config.writeTo(file);
+		JsonConfig read = new JsonConfig();
+		read.readFrom(file);
+		System.out.println("config: " + config);
+		System.out.println("read: " + read);
+	}
 
 	@Test
 	public void testWrite() throws IOException {
@@ -47,7 +62,7 @@ public class JsonConfigTest {
 	public void testFancyWriter() throws IOException {
 		try (Writer fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8))) {
 			FancyJsonWriter jsonWriter = new FancyJsonWriter();
-			jsonWriter.setNewline("\r");
+			jsonWriter.setNewline("\r\n");
 			jsonWriter.setIndent("    ");
 			jsonWriter.writeConfig(config, fileWriter);
 		}//finally closes the writer
