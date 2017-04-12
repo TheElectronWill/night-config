faa5c209980b88db0da073f0437220a5c6ec8ed6
TheElectronWill <theelectronwill@gmail.com>
2017-04-12 19:45:51 +0200

    core: Add FakeUnmodifiableCommentedConfig

---
+++ core/src/main/java/com/electronwill/nightconfig/core/utils/FakeUnmodifiableCommentedConfig.java
@@ -0,0 +1,78 @@
+package com.electronwill.nightconfig.core.utils;
+
+import com.electronwill.nightconfig.core.UnmodifiableCommentedConfig;
+import com.electronwill.nightconfig.core.UnmodifiableConfig;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A fake UnmodifiableCommentedConfig that wraps a config that doesn't support comments.
+ *
+ * @author TheElectronWill
+ */
+public final class FakeUnmodifiableCommentedConfig implements UnmodifiableCommentedConfig {
+	private final UnmodifiableConfig config;
+
+	public FakeUnmodifiableCommentedConfig(UnmodifiableConfig config) {
+		this.config = config;
+	}
+
+	@Override
+	public String getComment(List<String> path) {
+		return null;
+	}
+
+	@Override
+	public boolean containsComment(List<String> path) {
+		return false;
+	}
+
+	@Override
+	public Set<? extends Entry> entrySet() {
+		return new TransformingSet<>(config.entrySet(), FakeCommentedEntry::new, o -> null, o -> o);
+	}
+
+	@Override
+	public <T> T getValue(List<String> path) {
+		return config.getValue(path);
+	}
+
+	@Override
+	public boolean containsValue(List<String> path) {
+		return config.containsValue(path);
+	}
+
+	@Override
+	public int size() {
+		return config.size();
+	}
+
+	@Override
+	public Map<String, Object> valueMap() {
+		return config.valueMap();
+	}
+
+	private static final class FakeCommentedEntry implements Entry {
+		private final UnmodifiableConfig.Entry entry;
+
+		private FakeCommentedEntry(UnmodifiableConfig.Entry entry) {
+			this.entry = entry;
+		}
+
+		@Override
+		public String getComment() {
+			return null;
+		}
+
+		@Override
+		public String getKey() {
+			return entry.getKey();
+		}
+
+		@Override
+		public <T> T getValue() {
+			return entry.getValue();
+		}
+	}
+}
