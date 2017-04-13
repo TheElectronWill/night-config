3d1ea68ad579c2554736c22f18e4fdbfaebeb5a7
TheElectronWill <theelectronwill@gmail.com>
2017-04-13 16:42:16 +0200

    hocon: Constructor HoconConfig(UnmodifiableCommentedConfig) + documentation

--- hocon/src/main/java/com/electronwill/nightconfig/hocon/HoconConfig.java
+++ hocon/src/main/java/com/electronwill/nightconfig/hocon/HoconConfig.java
@@ -2,6 +2,7 @@ package com.electronwill.nightconfig.hocon;
 
 import com.electronwill.nightconfig.core.AbstractCommentedConfig;
 import com.electronwill.nightconfig.core.SimpleConfig;
+import com.electronwill.nightconfig.core.UnmodifiableCommentedConfig;
 import com.electronwill.nightconfig.core.UnmodifiableConfig;
 import com.electronwill.nightconfig.core.io.FileConfig;
 import java.io.File;
@@ -13,17 +14,44 @@ import java.util.Map;
  * @author TheElectronWill
  */
 public final class HoconConfig extends AbstractCommentedConfig implements FileConfig {
+	/**
+	 * Creates an empty HoconConfig.
+	 */
 	public HoconConfig() {}
 
+	/**
+	 * Creates an HoconConfig backed by the specified Map.
+	 *
+	 * @param valuesMap the map containing the values
+	 */
 	public HoconConfig(Map<String, Object> valuesMap) {
 		super(valuesMap);
 	}
 
+	/**
+	 * Creates an HoconConfig that is a copy of the given config.
+	 *
+	 * @param toCopy the config to copy
+	 */
 	public HoconConfig(UnmodifiableConfig toCopy) {
 		super(toCopy);
 	}
 
-	public HoconConfig(AbstractCommentedConfig toCopy){
+	/**
+	 * Creates an HoconConfig that is a copy of the given config.
+	 *
+	 * @param toCopy the config to copy
+	 */
+	public HoconConfig(UnmodifiableCommentedConfig toCopy) {
+		super(toCopy);
+	}
+
+	/**
+	 * Creates an HoconConfig that is a copy of the given config.
+	 *
+	 * @param toCopy the config to copy
+	 */
+	public HoconConfig(AbstractCommentedConfig toCopy) {
 		super(toCopy);
 	}
 
