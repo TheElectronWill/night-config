70e7eb9ef1ca19dc41ebb74af3ffcf97fcc63917
TheElectronWill <theelectronwill@gmail.com>
2017-04-19 16:25:20 +0200

    core: Add utils.CommentedConfigWrapper

---
+++ core/src/main/java/com/electronwill/nightconfig/core/utils/CommentedConfigWrapper.java
@@ -0,0 +1,47 @@
+package com.electronwill.nightconfig.core.utils;
+
+import com.electronwill.nightconfig.core.CommentedConfig;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author TheElectronWill
+ */
+public abstract class CommentedConfigWrapper<C extends CommentedConfig> extends ConfigWrapper<C>
+		implements CommentedConfig {
+
+	protected CommentedConfigWrapper(C config) {
+		super(config);
+	}
+
+	@Override
+	public String getComment(List<String> path) {
+		return config.getComment(path);
+	}
+
+	@Override
+	public boolean containsComment(List<String> path) {
+		return config.containsComment(path);
+	}
+
+	@Override
+	public String setComment(List<String> path, String comment) {
+		return config.setComment(path, comment);
+	}
+
+	@Override
+	public String removeComment(List<String> path) {
+		return config.removeComment(path);
+	}
+
+	@Override
+	public Map<String, String> commentMap() {
+		return config.commentMap();
+	}
+
+	@Override
+	public Set<? extends CommentedConfig.Entry> entrySet() {
+		return config.entrySet();
+	}
+}
