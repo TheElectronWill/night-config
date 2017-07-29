510e7fa7e07fdf6d375fd867165f37fc0c41726b
TheElectronWill <theelectronwill@gmail.com>
2017-07-29 20:02:26 +0200

    core: Commit CommentedFileConfig

---
+++ core/src/main/java/com/electronwill/nightconfig/core/io/CommentedFileConfig.java
@@ -0,0 +1,11 @@
+package com.electronwill.nightconfig.core.io;
+
+import com.electronwill.nightconfig.core.CommentedConfig;
+import java.io.File;
+
+/**
+ * @author TheElectronWill
+ */
+public interface CommentedFileConfig extends CommentedConfig, FileConfig {
+
+}
