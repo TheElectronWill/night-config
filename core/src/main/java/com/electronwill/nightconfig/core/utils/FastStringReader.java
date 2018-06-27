4851b9d937a270c32e5851dc9d279facaaafc8d3
Guillaume R <theelectronwill@gmail.com>
2018-06-27 02:40:56 +0200

    Important fix for FastStringReader

--- core/src/main/java/com/electronwill/nightconfig/core/utils/FastStringReader.java
+++ core/src/main/java/com/electronwill/nightconfig/core/utils/FastStringReader.java
@@ -5,25 +5,41 @@ import java.util.Objects;
 
 /**
  * A fast, unsynchronized, simple Reader that reads the content of a String.
+ *
  * @author TheElectronWill
  */
 public final class FastStringReader extends Reader {
 	private final String str;
+	private final int lim;
 	private int cursor = 0, mark;
+
+	public FastStringReader(String str, int lim) {
+		if (lim > str.length() || lim < 0) {
+			throw new IllegalArgumentException("Invalid limit " + lim + ": must be >= 0 and < str.length()");
+		}
+		this.str = Objects.requireNonNull(str, "The string must not be null.");
+		this.lim = lim;
+	}
 
 	public FastStringReader(String str) {
 		this.str = Objects.requireNonNull(str, "The string must not be null.");
+		this.lim = str.length();
 	}
 
 	@Override
 	public int read() {
-		return str.charAt(cursor++);
+		return cursor < lim ? str.charAt(cursor++) : -1;
 	}
 
 	@Override
 	public int read(char[] cbuf, int off, int len) {
-		str.getChars(off, off + len, cbuf, 0);
-		cursor += len;
+		if (cursor == lim) {
+			return -1;
+		}
+		len = Math.min(len, lim - cursor);
+		int srcEnd = cursor + len;
+		str.getChars(cursor, srcEnd, cbuf, off);
+		cursor = srcEnd;
 		return len;
 	}
 
