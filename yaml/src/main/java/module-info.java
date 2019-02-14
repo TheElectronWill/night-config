516fa91e922beef185aa0db6fa336eea025e50a7
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-02-14 17:35:59 +0100

    Added missing dependency

--- yaml/src/main/java/module-info.java
+++ yaml/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.electronwill.night.config.yaml {
 	requires transitive com.electronwill.night.config.core;
+	requires java.sql;
 	requires snakeyaml;
 
 	exports com.electronwill.nightconfig.yaml;
