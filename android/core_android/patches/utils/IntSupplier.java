fcabd1913bb5ea7487ec51e8862dd235c2dd025e
Guillaume R <theelectronwill@gmail.com>
2019-03-02 00:58:46 +0100

    Implement IntSupplier and LongSupplier for old android versions

---
+++ android/core_android/patches/utils/IntSupplier.java
@@ -0,0 +1,6 @@
+package com.electronwill.nightconfig.core.utils;
+
+/** IntSupplier interface for android below api 24 */
+public interface IntSupplier {
+	int getAsInt();
+}
---
+++ android/core_android/patches/utils/LongSupplier.java
@@ -0,0 +1,6 @@
+package com.electronwill.nightconfig.core.utils;
+
+/** LongSupplier interface for android below api 24 */
+public interface LongSupplier {
+	long getAsLong();
+}
