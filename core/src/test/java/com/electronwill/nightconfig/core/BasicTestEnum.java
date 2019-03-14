1eef055ec6e2da3b6216daf354b71d3848e7cc70
Guillaume R <theelectronwill@gmail.com>
2019-03-14 19:59:02 +0100

    Test enums with yaml

---
+++ core/src/test/java/com/electronwill/nightconfig/core/BasicTestEnum.java
@@ -0,0 +1,5 @@
+package com.electronwill.nightconfig.core;
+
+public enum BasicTestEnum {
+	A,B,C
+}
--- yaml/src/test/java/yaml/YamlTest.java
+++ yaml/src/test/java/yaml/YamlTest.java
@@ -1,19 +1,22 @@
 package yaml;
 
+import com.electronwill.nightconfig.core.BasicTestEnum;
 import com.electronwill.nightconfig.core.Config;
+import com.electronwill.nightconfig.core.TestEnum;
 import com.electronwill.nightconfig.core.file.FileNotFoundAction;
 import com.electronwill.nightconfig.core.io.ParsingMode;
 import com.electronwill.nightconfig.core.io.WritingMode;
 import com.electronwill.nightconfig.yaml.YamlFormat;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.TypeDescription;
+import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.File;
 
 import static com.electronwill.nightconfig.core.NullObject.NULL_OBJECT;
 import static com.electronwill.nightconfig.core.file.FileNotFoundAction.THROW_ERROR;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class YamlTest {
 
@@ -27,6 +30,7 @@ public class YamlTest {
 		config.set("string", "this is a string");
 		config.set("sub.null", null);
 		config.set("sub.nullObject", NULL_OBJECT);
+		config.set("enum", BasicTestEnum.A); // complex enums doesn't appear to work with SnakeYAML
 
 		System.out.println("Config: " + config);
 		System.out.println("classOf[sub] = " + config.get("sub").getClass());
@@ -42,7 +46,8 @@ public class YamlTest {
 		assertNull(parsed.get("sub.null"));
 		assertNull(parsed.get("sub.nullObject"));
 		assertSame(NULL_OBJECT, parsed.valueMap().get("null"));
-		assertSame(NULL_OBJECT,parsed.valueMap().get("nullObject"));
+		assertSame(NULL_OBJECT,	parsed.valueMap().get("nullObject"));
+		assertEquals(BasicTestEnum.A, parsed.getEnum("enum", BasicTestEnum.class));
 
 		Assertions.assertEquals(config, parsed, "Error: written != parsed");
 	}
