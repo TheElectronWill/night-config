3c5aa02bd70f62bdff30ba3faa4c63eeb61cdab2
Guillaume R <theelectronwill@gmail.com>
2019-09-29 19:57:13 +0200

    core: Optimize StringUtils

--- core/src/main/java/com/electronwill/nightconfig/core/utils/StringUtils.java
+++ core/src/main/java/com/electronwill/nightconfig/core/utils/StringUtils.java
@@ -1,8 +1,8 @@
 package com.electronwill.nightconfig.core.utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.electronwill.nightconfig.core.io.CharsWrapper;
+
+import java.util.*;
 
 /**
  * Fast string utilities.
@@ -14,6 +14,15 @@ public final class StringUtils {
 	private StringUtils() {}// Utility class that can't be constructed
 
 	/**
+	 * Splits a String at each dot. Shortcut for {@code split(path, '.')}
+	 * @param path the string to split
+	 * @return a list containing the splitted parts
+	 */
+	public static String[] splitPath(String path) {
+		return split(path, '.');
+	}
+
+	/**
 	 * Splits a String around each occurence of the specified character. The result is <b>not</b>
 	 * the same as {@link String#split(String)}. In particular, this method never returns an
 	 * empty list.
@@ -21,22 +30,32 @@ public final class StringUtils {
 	 * Examples:
 	 * <ul>
 	 * <li>{@code split("a.b.c", '.')} gives {@code ["a", "b", "c"]}
-	 * <li>{@code split("", '.')} gives {@code [""]} (a list containing the empty string)
-	 * <li>{@code split(".", '.')} gives {@code ["", ""]} (a list containing two empty strings)
-	 * <li>{@code split("..", '.')} gives {@code ["", "", ""]} (a list containing three empty
-	 * strings)
-	 * <li>{@code split(".a...b.", '.')} gives {@code ["", "a", "", "", "b", ""]} (a list containing
-	 * an empty string, the string "a", two empty strings, the string "b", and an empty string)
+	 * <li>{@code split("", '.')} gives {@code [""]} (a list containing 1 empty string)
+	 * <li>{@code split(".", '.')} gives {@code ["", ""]} (a list containing 2 empty strings)
+	 * <li>{@code split("..", '.')} gives {@code ["", "", ""]} (a list containing 3 empty strings)
+	 * <li>{@code split(".a...b.", '.')} gives {@code ["", "a", "", "", "b", ""]}
 	 * </ul>
 	 *
 	 * @param str the String to split
 	 * @param sep the separator to use
 	 * @return a non-empty list of strings
 	 */
-	public static List<String> split(String str, char sep) {
-		List<String> list = new ArrayList<>(8);// usually the paths are small
-		split(str, sep, list);
-		return list;
+	public static String[] split(String str, char sep) {
+		String[] parts = new String[8];
+		int partIdx = 0;
+		int partBegin = 0;
+		int partEnd;
+		while ((partEnd = str.indexOf(sep, partBegin)) != -1) {
+			String part = str.substring(partBegin, partEnd);
+			if (partIdx == parts.length) {
+				parts = Arrays.copyOf(parts, parts.length << 1);
+			}
+			parts[partIdx++] = part;
+			partBegin = partEnd + 1;
+		}
+		String[] result = Arrays.copyOf(parts, partIdx+1); // +1 for the last part
+		result[result.length-1] = str.substring(partBegin); // add the last part
+		return result;
 	}
 
 	/**
@@ -57,58 +76,86 @@ public final class StringUtils {
 	 *
 	 * @param str  the String to split
 	 * @param sep  the separator to use
-	 * @param list the list where to put the results
+	 * @param dst the list where to put the results
 	 */
-	public static void split(String str, char sep, List<String> list) {
+	public static void split(String str, char sep, List<String> dst) {
 		int pos0 = 0;
 		for (int i = 0; i < str.length(); i++) {
 			if (str.charAt(i) == sep) {// separator found
-				list.add(str.substring(pos0, i));
+				dst.add(str.substring(pos0, i));
 				pos0 = i + 1;
 			}
 		}
-		list.add(str.substring(pos0));// adds the last part
+		dst.add(str.substring(pos0));// adds the last part
 	}
 
 	/**
-	 * Splits a String around each occurence of LF and CRLF. If the String is null or empty, then
-	 * an empty list is returned.
+	 * Joins an array of strings with dots.
+	 * This is the opposite of {@link #splitPath(String)}.
 	 *
-	 * @param str the String to split
-	 * @return a list of strings, may be empty
+	 * @return the result of the concatenation of each element with a '.' between 2 elements
 	 */
-	public static List<String> splitLines(String str) {
-		if (str == null || str.isEmpty()) {
-			return Collections.emptyList();
+	public static String joinPath(String[] strings) {
+		return join(strings, '.');
+	}
+
+	/**
+	 * Like {@link java.lang.String#join(java.lang.CharSequence, java.lang.CharSequence...)} but
+	 * optimized for single-char delimiters.
+	 */
+	public static String join(String[] strings, char delimiter) {
+		CharsWrapper.Builder builder = new CharsWrapper.Builder(32);
+		join(strings, delimiter, builder);
+		return builder.toString();
+	}
+
+	public static void join(String[] strings, char delimiter, CharsWrapper.Builder dst) {
+		int lastIndex = strings.length - 1;
+		for (int i = 0; i < lastIndex; i++) {
+			dst.append(strings[i]).append(delimiter);
 		}
-		List<String> list = new ArrayList<>(8);
-		splitLines(str, list);
-		return list;
+		dst.append(strings[lastIndex]);
 	}
 
 	/**
-	 * Splits a String around each occurence of LF and CRLF, and puts the result in the given list.
+	 * Iterates over the lines of a String.
+	 * The lines may be separated by '\n' (LF) or "\r\n" (CRLF).
 	 *
-	 * @param str  the String to split
-	 * @param list the list where to put the result
+	 * @param str the string to iterate over
+	 * @return an iterator over the lines of the string
 	 */
-	public static void splitLines(String str, List<String> list) {
-		int pos0 = 0;
-		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			if (ch == '\n') { // LF
-				list.add(str.substring(pos0, i));
-				pos0 = i + 1;
-			} else if (ch == '\r') { // CR or CRLF
-				list.add(str.substring(pos0, i));
-				int next = i + 1;
-				if (next < str.length() && str.charAt(next) == '\n') { // CRLF
-					i += 1; // Skips the LF that follows the CR
-				} // condition here instead of boolean flag to make the LF way simpler and (marginally) faster
-				pos0 = i + 1;
+	public static Iterator<String> iterateLines(String str) {
+		return new Iterator<String>() {
+			private int lineStart = 0;
+			private int lineEnd = 0;
+			private int sepSize = 0;
+
+			{ findNextEnd(); }
+
+			private void findNextEnd() {
+				lineEnd = str.indexOf('\n', lineStart);
+				if (lineEnd >= 1 && str.charAt(lineEnd - 1) == '\r') {
+					// Handle CRLF
+					lineEnd -= 1;
+					sepSize = 2;
+				} else {
+					// Handle LF
+					sepSize = 1;
+				}
 			}
-		}
-		String lastPart = str.substring(pos0);
-		list.add(lastPart);
+
+			@Override
+			public boolean hasNext() {
+				return lineEnd != -1;
+			}
+
+			@Override
+			public String next() {
+				String line = str.substring(lineStart, lineEnd);
+				lineStart = lineEnd + sepSize;
+				findNextEnd();
+				return line;
+			}
+		};
 	}
 }
