3ccddb20b90df0323e148da9e7e381d5aee157b0
Guillaume R <theelectronwill@gmail.com>
2019-11-01 19:29:27 +0100

    Rewrite Tokenizer

--- core/src/main/java/com/electronwill/nightconfig/core/impl/Tokenizer.java
+++ core/src/main/java/com/electronwill/nightconfig/core/impl/Tokenizer.java
@@ -1,8 +1,16 @@
 package com.electronwill.nightconfig.core.impl;
 
-import java.io.IOException;
+public interface Tokenizer<Token> {
+	Token next();
 
-@FunctionalInterface
-public interface Tokenizer<I> {
-	void tokenize(I in, TokenHandler out) throws IOException;
+	<T> T genericValue();
+	CharSequence textValue();
+
+	int intValue();
+	long longValue();
+	double doubleValue();
+	boolean booleanValue();
+
+	int line();
+	int column();
 }
