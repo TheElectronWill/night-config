e4e1398aaed6cb4590f224f275ca15f5b987311a
tterrag <tterrag1098@gmail.com>
2019-11-01 05:43:26 -0400

    Fix NPE when a concurrent commented map encounters a null comment (#71)

--- core/src/main/java/com/electronwill/nightconfig/core/AbstractCommentedConfig.java
+++ core/src/main/java/com/electronwill/nightconfig/core/AbstractCommentedConfig.java
@@ -91,7 +91,10 @@ public abstract class AbstractCommentedConfig extends AbstractConfig implements 
 		final int lastIndex = path.size() - 1;
 		final String lastKey = path.get(lastIndex);
 		if (lastIndex == 0) {
-			return commentMap.put(lastKey, comment);
+			if (comment != null) {
+				return commentMap.put(lastKey, comment);
+			}
+			return commentMap.remove(lastKey);
 		}
 		final List<String> parentPath = path.subList(0, lastIndex);
 		Object parent = getRaw(parentPath);
