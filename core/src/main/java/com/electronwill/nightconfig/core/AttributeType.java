21bb31657cc716394723cc65225f5da98574b719
Guillaume Raffin <theelectronwill@gmail.com>
2020-04-23 14:57:54 +0200

    Make AttributeType a class

--- core/src/main/java/com/electronwill/nightconfig/core/AttributeType.java
+++ core/src/main/java/com/electronwill/nightconfig/core/AttributeType.java
@@ -1,10 +1,18 @@
 package com.electronwill.nightconfig.core;
 
 /**
- * Additional attribute attached to a configuration entry.
+ * Represents a type of attribute attached to a configuration entry.
  *
- * @param <T> the attribute's type
+ * @param <T> the type of the attribute's value
  */
-public interface AttributeType<T> {
-	String getName();
+public class AttributeType<T> {
+	private final String name;
+
+	public AttributeType(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 }
--- core/src/main/java/com/electronwill/nightconfig/core/StandardAttributes.java
+++ core/src/main/java/com/electronwill/nightconfig/core/StandardAttributes.java
@@ -4,29 +4,30 @@ public final class StandardAttributes {
 	private StandardAttributes() {}
 
 	/** Entry's value */
-	public static final AttributeType<Object> VALUE = () -> "value";
+	public static final AttributeType<Object> VALUE = new AttributeType<>("value");
 
-	/** Entry's comment */
-	public static final AttributeType<String> COMMENT = () -> "comment";
+	/** Entry's comment (only some config formats support comments).
+	 * @see ConfigFormat#supportsComments */
+	public static final AttributeType<String> COMMENT = new AttributeType<>("comment");
 
 	/** Number's base/radix, for instance 16 will save the value in hexadecimal (if possible) */
-	public static final AttributeType<Integer> NUMBER_BASE = () -> "numberBase";
+	public static final AttributeType<Integer> NUMBER_BASE = new AttributeType<>("numberBase");
 
 	/**
 	 * Forces a config entry to be serialized as multiple lines. For instance, if applied to
 	 * a list, forces each element to be written on a separate line (if supported by the format).
 	 * <p>
-	 * If neither {@code FORCE_MULTILINE} nor {@link #FORCE_SINGLELINE} is specified, the
+	 * If neither {@link #FORCE_MULTILINE} nor {@link #FORCE_SINGLELINE} is specified, the
 	 * {@code ConfigWriter} is free to decide.
 	 */
-	public static final AttributeType<Boolean> FORCE_MULTILINE = () -> "forceMultiline";
+	public static final AttributeType<Boolean> FORCE_MULTILINE = new AttributeType<>("forceMultiline");
 
 	/**
 	 * Forces a config entry to be serialized as a single line. This is the opposite of
 	 * {@link #FORCE_MULTILINE}.
 	 * <p>
-	 * If neither {@link #FORCE_MULTILINE} nor {@code FORCE_SINGLELINE} is specified, the
+	 * If neither {@link #FORCE_MULTILINE} nor {@link #FORCE_SINGLELINE} is specified, the
 	 * {@code ConfigWriter} is free to decide.
 	 */
-	public static final AttributeType<Boolean> FORCE_SINGLELINE = () -> "forceMultiline";
+	public static final AttributeType<Boolean> FORCE_SINGLELINE = new AttributeType<>("forceMultiline");
 }
