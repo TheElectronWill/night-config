3cb70602d4f25a42cbb83ccaec05eb6a4296a984
Stein-N <nico-stein-98@outlook.de>
2024-02-29 18:34:29 +0100

    Revert "changing Category to Header"
    This reverts commit 3b19f59ff28d7825b4fe9673c42c4774ab44d43e.

--- toml/src/test/java/com/electronwill/nightconfig/toml/TomlWriterTest.java
+++ toml/src/test/java/com/electronwill/nightconfig/toml/TomlWriterTest.java
@@ -285,20 +285,20 @@ public class TomlWriterTest {
 	}
 
 	@Test
-	public void writeHeaderAndValueComments() {
+	public void writeCategoryAndValueComments() {
 		CommentedConfig config = CommentedConfig.inMemory();
 
-		config.setComment("Header", "Header Comment");
-		config.setComment("Header.key", "Value Comment");
+		config.setComment("Category", "Category Comment");
+		config.setComment("Category.key", "Value Comment");
 
-		config.set("Header.key", "value");
+		config.set("Category.key", "value");
 
 		TomlWriter writer = new TomlWriter();
 		String written = writer.writeToString(config);
 
 		System.out.println(written);
-		assertEquals(join("#Header Comment",
-							"[Header]",
+		assertEquals(join("#Category Comment",
+							"[Category]",
 							"\t#Value Comment",
 							"\tkey = \"value\"",
 							""), written);
