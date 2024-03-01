b0cf7fc1d0aaf6088c8f944a3b3da98950b553a4
Guillaume Raffin <theelectronwill@gmail.com>
2024-03-01 21:09:53 +0100

    core: Add package-info to serde package

---
+++ core/src/main/java/com/electronwill/nightconfig/core/serde/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides tools for serializing values to configurations, and deserializing configurations to Java objects.
+ * 
+ * @see com.electronwill.nightconfig.core.serde.ObjectSerializer
+ * @see com.electronwill.nightconfig.core.serde.ObjectDeserializer
+ */
+package com.electronwill.nightconfig.core.serde;
