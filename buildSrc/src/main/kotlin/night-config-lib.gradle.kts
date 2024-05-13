2ca3b8c28c1acabec359a94cd2cbf75f35cc0d6b
Guillaume Raffin <theelectronwill@gmail.com>
2024-05-13 17:04:59 +0200

    Bump version to 3.7.1

--- buildSrc/src/main/kotlin/night-config-lib.gradle.kts
+++ buildSrc/src/main/kotlin/night-config-lib.gradle.kts
@@ -111,7 +111,7 @@ project.afterEvaluate {
 
 // Set project metadata for publishing
 group = "com.electronwill.night-config"
-version = "3.7.0"
+version = "3.7.1"
 
 // Publish the library as a Maven artifact.
 publishing {
